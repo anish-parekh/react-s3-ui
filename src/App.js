b8345323a6ace83feb909f380a25790c80268ad2
Anish Parekh <anishparekh02112001@gmail.com>
2023-03-27 23:59:37 +0530

    removed the select files options from the bottom section and other ui changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,34 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 import AWS from 'aws-sdk';
 import Papa from 'papaparse';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import staticDataCSVFile from './prices_for_postman_test_final.csv';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 
 const s3_bucket ='YOUR_BUCKET_NAME_HERE';
@@ -53,36 +81,55 @@ function App() {
   };
 
 
-  const handleFlaskFileInput = (e) => {
-    setSelectedFlaskFile(e.target.files[0]);
-  };
-
-
   const uploadFileToFlaskEndpoint = (event) => {
-    const formData = new FormData();
-    formData.append('file', selectedFlaskFile);
+    // const formData = new FormData();
+    // formData.append('file', selectedFlaskFile);
 
-    fetch('http://127.0.0.1:5000/endpoint', {
-      method: 'POST',
-      headers: {
-        'SECRET_KEY': {secretKey},
-      },    
-      body: formData,
+    fetch('http://127.0.0.1:5000/predict', {
+      method: 'GET',
     })
     .then((response) => {
       return response.text();
     })
     .then((text) => {
         text=JSON.parse(text);
+        // text.pltImg = <img src={text.pltImg} alt="No image found!"></img>;
+        // const imgURL = text.pltImg;
+        const imgURL = 'https://marketsworkshop.s3.amazonaws.com/mldata/dataplot.png';
+        const img = document.createElement('img');
+        img.src = imgURL;
+        img.id = "plot";
+        console.log(img);
+        document.body.appendChild(img);
+
+        text.pltImg = img;
+        // fetch(imgURL)
+        // .then(response => response.blob())
+        // .then(blob => {
+        //   const reader = new FileReader();
+        //   reader.readAsDataURL(blob);
+        //   reader.onloadend = () => {
+        //     const img = document.createElement('img');
+        //     img.src = reader.result;
+        //     document.body.appendChild(img);
+
+        //     text.pltImg = reader.result;
+        //   }
+        // })
+        console.log(text);
         setFlaskResp(JSON.stringify(text,null,2));
         let res = document.getElementById("flask-resp");
+        let imageplot = document.getElementById("plot");
+
         if(res.style.display === "none")
         {
           res.style.display = "inline";
+          imageplot.style.display = "inline"
         }
         else
         {
           res.style.display = "none";
+          imageplot.style.display = "none";
         }
         console.log(text);
     })
@@ -92,15 +139,28 @@ function App() {
 
   };
 
-  const staticDataHandleUpload = (e) => {
-    const file = e.target.files[0];
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        setStaticData(results.data);
-      },
-    });
-  };
+  // const file = fetch('D:/WFI/WI/React_App_S3_connection-files/prices_for_postman_test_final.csv');
+  // var blob = new Blob([file], { type: "text/plain" });
+
+  Papa.parse(staticDataCSVFile, {
+    download: true,
+    header: true,
+    complete: (results) => {
+      setStaticData(results.data);
+      console.log(results.data)
+    },
+  });
+    
+  // const staticDataHandleUpload = (e) => {
+  //   Papa.parse(staticDataCSVFile, {
+  //     download: true,
+  //     header: true,
+  //     complete: (results) => {
+  //       setStaticData(results.data);
+  //       console.log(results.data)
+  //     },
+  //   });
+  // };
   
   function showStaticData() {
     let table = document.getElementById("static-data-table");
@@ -134,67 +194,58 @@ function App() {
         <div>Uploading {progress}% ...</div>
       </div>
 
-      <div className='section2'>
+      <div className='section2' style={{display: "flex"}}>
         <div>
-        <label htmlFor="static-data-file-upload">
-            <Button component="span">
-              Select File
+          <div style={{margin: "10px"}}>
+            {/* <label htmlFor="static-data-file-upload">
+                <Button component="span">
+                  Select File
+                </Button>
+            </label>
+            <input 
+              id="static-data-file-upload"
+              accept='.csv'
+              type="file" 
+              style={{ display: 'none' }}
+              onChange={staticDataHandleUpload} 
+            /> */}
+
+            <Button onClick={showStaticData} variant="contained" color="secondary">
+              Test
             </Button>
-        </label>
-        <input 
-          id="static-data-file-upload"
-          accept='.csv'
-          type="file" 
-          style={{ display: 'none' }}
-          onChange={staticDataHandleUpload} 
-        />
-
-        <Button onClick={showStaticData} variant="contained" color="secondary">
-          Test
-        </Button>
-          <table id="static-data-table" style={{display: "none"}}>
-            <thead>
-              <tr>
-                {staticData.length > 0 &&
-                  Object.keys(staticData[0]).map((key) => <th key={key}>{key}</th>)}
-              </tr>
-            </thead>
-            <tbody>
-              {staticData.map((row, index) => (
-                <tr key={index}>
-                  {Object.values(row).map((value, index) => (
-                    <td key={index}>{value}</td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        
-        </div>
-
-        <label htmlFor="file-upload-to-flask">
-            <Button component="span">
-              Select File
-            </Button>
-        </label>
-          <input
-            id="file-upload-to-flask"
-            type="file"
-            accept=".csv"
-            style={{ display: 'none' }}
-            onChange={handleFlaskFileInput}
-          />
-
+          </div>
+          <div style={{margin: "10px"}}>            
             <Button onClick={() => uploadFileToFlaskEndpoint(selectedFlaskFile)} variant="contained" color="primary" component="span">
               Process Data
             </Button>
+          </div>
+        </div>
 
-            <div id='flask-resp' style={{display: "none"}}>
-              <pre>{flaskResp}</pre>
-            </div>
+        <TableContainer component={Paper}>
+          <Table id="static-data-table" style={{display: "none"}} sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                {staticData.length > 0 &&
+                  Object.keys(staticData[0]).map((key) => <StyledTableCell key={key} style={{backgroundColor: "black", color: "white"}}>{key.toUpperCase()}</StyledTableCell>)}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {staticData.map((row, index) => (
+                <StyledTableRow key={index}>
+                  {Object.values(row).map((value, index) => (
+                    <StyledTableCell  key={index}>{value}</StyledTableCell>
+                  ))}
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+          
 
+              <div id='flask-resp' style={{display: "none"}}>
+                <pre>{flaskResp}</pre>
+              </div>
       </div>
-      
     </div>
   );
 }
--- src/app.py
+++ src/app.py
@@ -3,22 +3,19 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = 'secret_key'
-# app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# cors = CORS(app, resources={r"/endpoint": {"origins": "http://localhost:3000/"}})
+cors = CORS(app, resources={r"/predict": {"origins": "http://localhost:3000/"}})
 
-@app.route('/predict', methods=['POST'])
-# @cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
+@app.route('/predict', methods=['GET'])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
 def predict():
-    file = request.files['file']
-    return [
-		{"Actual":134.40,"Predict":129.61},
-		{"Actual":126.59,"Predict":121.55},
-		{"Actual":106.86,"Predict":108.37},
-		{"Actual":138.95,"Predict":125.43},
-		{"Actual":119.49,"Predict":124.50}
-	   ]
+    return {'data': [{'instrumentId': 'abcd1', 'actual': 82.3, 'predicted': 95.3, 'diff': 5.3}, 
+		    {'instrumentId': 'abcd2', 'actual': 112.3, 'predicted': 182.3, 'diff': 2.3}, 
+		    {'instrumentId': 'abcd3', 'actual': 514.3, 'predicted': 519.3, 'diff': 9.3}, 
+		    {'instrumentId': 'abcd4', 'actual': 128.3, 'predicted': 140.3, 'diff': 14.3}],
+	   'pltImg': 'http://54.152.131.225:5000/dataplot.png'}
 
 if __name__ == '__main__':
     app.run(debug=True)
